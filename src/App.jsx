c34c2efd9180ce93fd249772d377e546fe940800
Felipe <felipemsoares22@gmail.com>
2024-09-27 11:01:44 -0300

    batata

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { MyForm } from './components/Formulario'
+import Garage from './components/Listas'
+
+function App() {
+  return (
+    <p>
+  <Garage/>
+  <MyForm/>
+    </p>
+)
+}
+
+export default App
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function MyForm() {
+  return (
+    <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+    </form>
+  )
+}
